0feafda720afa96056002f9c8eb3cf42b4858c00
daeldev <danielalmeida41263@gmail.com>
2024-03-17 01:27:31 -0300

    Initial commit

---
+++ Testegit/src/testegit/Testegit.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testegit;
+
+/**
+ *
+ * @author Josiel
+ */
+public class Testegit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       int teste = 10;   
+       for (int n = 0; n<=teste; n++){
+           System.out.println("- "+n);
+       }     
+    }  
+}
